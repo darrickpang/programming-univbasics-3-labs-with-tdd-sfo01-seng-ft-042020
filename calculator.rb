9e603c3928604704a2af6828fa26cd8c0f01dafe
Darrick Pang <dkypang@yahoo.com>
2020-03-26 19:51:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
 # Add your variables here
-first_number = 6
+first_number = 17
 print first_number
 
 second_number = 9
 print second_number
-sum = first_number + second_number
+sum = first_number - second_number
 print sum
